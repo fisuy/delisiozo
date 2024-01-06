6b184f70d6a218c068293d18af32176082a8b2a7
yusif <yusif.rustamli7@gmail.com>
2024-01-06 18:10:52 +0400

    bug fix

--- src/component/Navigation/index.jsx
+++ src/component/Navigation/index.jsx
@@ -1,39 +1,37 @@
-import React from 'react'
-import "./navigation.css"
-import Button from "../button"
-import Logo from '../logo'
-
-
-
+import React from "react";
+import "./navigation.css";
+import Button from "../Button";
+import Logo from "../logo";
 
 const Navigation = () => {
   return (
     <nav className="navigation">
-      <Logo/>
-        <ul className="ul">
-            <li>
-              <a className='active' href="#">Home</a>
-            </li>
-            <li>
-              <a href="">Menu</a>
-            </li>
-            <li>
-              <a href="">About us</a>
-            </li>
-            <li>
-              <a href="">Order online</a>
-            </li>
-            <li>
-              <a href="">Reservation</a>
-            </li>
-            <li>
-              <a href="">Contact us</a>
-            </li>
-        </ul>
-        <Button item={5} />
-        
-      </nav>
-  )
-}
+      <Logo />
+      <ul className="ul">
+        <li>
+          <a className="active" href="#">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="">Menu</a>
+        </li>
+        <li>
+          <a href="">About us</a>
+        </li>
+        <li>
+          <a href="">Order online</a>
+        </li>
+        <li>
+          <a href="">Reservation</a>
+        </li>
+        <li>
+          <a href="">Contact us</a>
+        </li>
+      </ul>
+      <Button item={5} />
+    </nav>
+  );
+};
 
-export default Navigation
+export default Navigation;
